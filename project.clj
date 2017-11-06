5f3ce48b3a3b3c1d4c47cfaee8b5e2101268c1c6
Greg Look <greg@greg-look.net>
2017-11-05 20:34:31 -0800

    Add coverage plugin and alias.

--- project.clj
+++ project.clj
@@ -4,10 +4,16 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
 
+  :aliases
+  {"coverage" ["cloverage"]}
+
   :deploy-branches ["master"]
   :pedantic? :abort
+
+  :plugins
+  [[lein-cloverage "1.0.10"]]
 
   :dependencies
   [[org.clojure/clojure "1.8.0"]
    [clojure-future-spec "1.9.0-beta4"]
-   [mvxcvi/multihash "2.0.1"]])
+   [mvxcvi/multihash "2.0.2"]])
