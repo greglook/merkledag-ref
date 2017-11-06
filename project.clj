783446736bfe6f3f65bb4a09f3207b45bf11bd5c
Greg Look <greg@greg-look.net>
2017-11-05 20:49:30 -0800

    Add codox config.

--- project.clj
+++ project.clj
@@ -16,4 +16,9 @@
   :dependencies
   [[org.clojure/clojure "1.8.0"]
    [clojure-future-spec "1.9.0-beta4"]
-   [mvxcvi/multihash "2.0.2"]])
+   [mvxcvi/multihash "2.0.2"]]
+
+  :codox
+  {:metadata {:doc/format :markdown}
+   :source-uri "https://github.com/greglook/merkledag-ref/blob/master/{filepath}#L{line}"
+   :output-path "target/doc/api"})
